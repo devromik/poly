c03caeaaf59216174d07de9bf1741ef6540846b8
devromik <dev.romik@gmail.com>
2016-03-23 17:13:06 +0300

    Implement evaluation of a Polynomial using Horner's rule

--- src/main/kotlin/net/devromik/poly/Polynomial.kt
+++ src/main/kotlin/net/devromik/poly/Polynomial.kt
@@ -4,7 +4,26 @@ package net.devromik.poly
  * @author Shulnyaev Roman
  */
 class Polynomial(val coeffs: Array<Double>) {
+
+    init {
+        require(coeffs.size > 0)
+    }
+
     val coeffCount: Int = coeffs.size
     fun coeff(i: Int): Double = coeffs[i]
     val degree: Int = coeffCount - 1
+
+    fun at(x: Double): Double {
+        if (coeffCount == 1) {
+            return coeff(0)
+        }
+
+        var r = coeff(coeffCount - 1)
+
+        for (i in coeffCount - 2 downTo 0) {
+            r = r * x + coeff(i)
+        }
+
+        return r
+    }
 }
--- src/test/kotlin/net/devromik/poly/PolynomialTest.kt
+++ src/test/kotlin/net/devromik/poly/PolynomialTest.kt
@@ -8,18 +8,49 @@ import org.junit.Test
  */
 class PolynomialTest {
 
+    companion object {
+        val EPSILON = 1e-10
+    }
+
     @Test fun hasCoefficients() {
         val p = Polynomial(arrayOf(1.0, 2.0, 3.0))
 
         assertArrayEquals(arrayOf(1.0, 2.0, 3.0), p.coeffs)
         assertEquals(p.coeffCount, 3)
-        assertEquals(1.0, p.coeff(0), 1e-15)
-        assertEquals(2.0, p.coeff(1), 1e-15)
-        assertEquals(3.0, p.coeff(2), 1e-15)
+        assertEquals(1.0, p.coeff(0), EPSILON)
+        assertEquals(2.0, p.coeff(1), EPSILON)
+        assertEquals(3.0, p.coeff(2), EPSILON)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun hasAtLeastOneCoefficient() {
+        Polynomial(arrayOf())
     }
 
     @Test fun hasDegree() {
         val p = Polynomial(arrayOf(1.0, 2.0, 3.0))
         assertEquals(2, p.degree)
+    }
+
+    @Test fun canBeEvaluated() {
+        var p = Polynomial(arrayOf(1.0, 2.0, 3.0))
+
+        assertEquals(1.0 + 2.0 * (-2.0) + 3.0 * (-2.0 * -2.0), p.at(-2.0), EPSILON)
+        assertEquals(1.0 + 2.0 * (-1.0) + 3.0 * (-1.0 * -1.0), p.at(-1.0), EPSILON)
+        assertEquals(1.0, p.at(0.0), EPSILON)
+        assertEquals(1.0 + 2.0 * (1.0) + 3.0 * (1.0 * 1.0), p.at(1.0), EPSILON)
+        assertEquals(1.0 + 2.0 * (2.0) + 3.0 * (2.0 * 2.0), p.at(2.0), EPSILON)
+
+        p = Polynomial(arrayOf(-1.0, 2.0, -3.0))
+
+        assertEquals(-1.0 + 2.0 * (-2.0) - 3.0 * (-2.0 * -2.0), p.at(-2.0), EPSILON)
+        assertEquals(-1.0 + 2.0 * (-1.0) - 3.0 * (-1.0 * -1.0), p.at(-1.0), EPSILON)
+        assertEquals(-1.0, p.at(0.0), EPSILON)
+        assertEquals(-1.0 + 2.0 * (1.0) - 3.0 * (1.0 * 1.0), p.at(1.0), EPSILON)
+        assertEquals(-1.0 + 2.0 * (2.0) - 3.0 * (2.0 * 2.0), p.at(2.0), EPSILON)
+
+        p = Polynomial(arrayOf(7.0))
+        assertEquals(7.0, p.at(-1.0), EPSILON)
+        assertEquals(7.0, p.at(1.0), EPSILON)
     }
 }
