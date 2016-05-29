4723eea35b3db71c10664c091b17762d60138fa8
devromik <dev.romik@gmail.com>
2016-05-29 17:58:48 +0300

    Refactoring

--- src/test/kotlin/net/devromik/poly/PairwiseSumsTest.kt
+++ src/test/kotlin/net/devromik/poly/PairwiseSumsTest.kt
@@ -61,6 +61,8 @@ class PairwiseSumsTest {
         a = intArrayOf(7, 0, 1, 2, 3, 1, 7, 8)
         b = intArrayOf(4, 5, 9, 3, 3, 5, 7, 1)
 
-        assertArrayEquals(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17), pairwiseSumsOfNonNegativeArrays(a, b))
+        assertArrayEquals(
+            intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17),
+            pairwiseSumsOfNonNegativeArrays(a, b))
     }
 }
